76edfd75c68ab8111f7a432c4c93ac9e74cc0540
Fengalf <14805375+Fengalf@users.noreply.github.com>
2023-11-06 17:43:06 +0100

    Fixed the visibility of the center line and scores

--- scoreboard.py
+++ scoreboard.py
@@ -11,6 +11,8 @@ class Scoreboard(Turtle):
         # setting attributes
         self.left_score = 0
         self.right_score = 0
+        self.size = 15
+        self.centerline = []
 
         # calling initializing methods
         self.create_center_line()
@@ -18,22 +20,26 @@ class Scoreboard(Turtle):
         self.right_counter = self.create_counter("right")
 
     def create_center_line(self):
-        for y_cord in range(int(WINDOW_HEIGHT/2)*-1, int(WINDOW_HEIGHT/2), -15):
-            self.penup()
-            self.color("white")
-            self.shape("square")
-            self.speed("fastest")
-            self.setposition((0, y_cord))
+        for y_cord in range(int(WINDOW_HEIGHT/2)-(self.size*2), (int(WINDOW_HEIGHT/2)-self.size)*-1, self.size*-2):
+            new_segment = Turtle()
+            new_segment.penup()
+            new_segment.turtlesize(stretch_wid=1.05, stretch_len=0.25)
+            new_segment.color("white")
+            new_segment.shape("square")
+            new_segment.speed("fastest")
+            new_segment.setposition((0, y_cord))
+            self.centerline.append(new_segment)
 
     def create_counter(self, orientation: str):
+        self.hideturtle()
+        self.turtlesize(self.size, self.size)
         self.penup()
         self.color("white")
-        self.hideturtle()
         self.shape("square")
         self.speed("fastest")
         if orientation == "left":
-            self.setpos((-30, WINDOW_HEIGHT))
-            self.write(self.left_score, font=("Arial", 15, "bold"))
+            self.setposition((-45, WINDOW_HEIGHT/2-(self.size*2)))
+            self.write(self.left_score, font=("Arial", self.size, "bold"))
         else:
-            self.setpos((30, WINDOW_HEIGHT))
-            self.write(self.right_score, font=("Arial", 15, "bold"))
+            self.setposition((40, WINDOW_HEIGHT/2-(self.size*2)))
+            self.write(self.right_score, font=("Arial", self.size, "bold"))
