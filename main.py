50da0d137d1425d8a70d02b7a52a596632c6ef61
Fengalf <14805375+Fengalf@users.noreply.github.com>
2023-11-06 19:21:04 +0100

    Adding needed config to run the game

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from character import Paddle
 from scoreboard import Scoreboard
+from ball import PongBall
 from turtle import Screen
 from time import sleep
 
 WINDOW_BG_COLOR = "grey"
-WINDOW_HEIGHT = 480
-WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 800
 REFRESH_RATE = 0.01
-MAX_SCORE = 10
+BALL_SIZE = 15
+MAX_SCORE = 1
 
 TESTING = False
 
@@ -19,6 +21,7 @@ def pong_game():
     scoreboard = Scoreboard()
     paddle_left = Paddle(orientation="left")
     paddle_right = Paddle(orientation="right")
+    pong_ball = PongBall()
 
     # Modifying the screen
     window.title("Pingu Pongo")
@@ -39,10 +42,14 @@ def pong_game():
     window.onkeypress(key="Escape", fun=window.bye)
 
     while TESTING or (MAX_SCORE > scoreboard.left_score and MAX_SCORE > scoreboard.right_score):
+        if paddle_left.distance(pong_ball.position()) <= BALL_SIZE or paddle_right.distance(pong_ball.position()) <= BALL_SIZE:
+            pong_ball.change_heading()
+        pong_ball.move()
         sleep(REFRESH_RATE)
         window.update()
 
     scoreboard.game_over()
+    window.exitonclick()
 
 
 if __name__ == "__main__":
